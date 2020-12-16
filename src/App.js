02c6ca84395ca97374a11609b4db4138674b7669
kquach229 <kquach229@gmail.com>
2020-12-16 14:44:28 -0500

    initial

--- src/App.js
+++ src/App.js
@@ -1,25 +1,30 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import { connect } from "react-redux";
 
-function App() {
+function App(props) {
+  console.log(props);
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <button onClick={props.decrement}>-</button>
+      <span>Count:{props.count}</span>
+      <button onClick={props.increment}>+</button>
+      <button onClick={props.reset}>Reset</button>
     </div>
   );
 }
 
-export default App;
+const mapStateToProps = (state) => {
+  return {
+    count: state.count,
+  };
+};
+
+const mapDispatchToProps = (dispatch) => {
+  return {
+    increment: () => dispatch({ type: "INCREMENT" }),
+    decrement: () => dispatch({ type: "DECREMENT" }),
+    reset: () => dispatch({ type: "RESET" }),
+  };
+};
+
+export default connect(mapStateToProps, mapDispatchToProps)(App);
